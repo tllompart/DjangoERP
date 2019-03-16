82f5e76d55795f3fa2440b4f997d2972817350d5
tllompart <tomeu@tllompart.es>
2019-03-16 14:44:19 +0100

    Party initial commit

--- party/models.py
+++ party/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Party(models.Model):
+    name = models.CharField(max_length=32)
+    full_name = models.CharField(max_length=60)
+    phone = models.CharField(max_length=9)
+    mobile = models.CharField(max_length=9)
+    fax = models.CharField(max_length=9)
+    email = models.CharField(max_length=60)
+    website = models.CharField(max_length=60)
--- party/tests.py
+++ party/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 
 class TestPage(TestCase):
     def test_party_page(self):
-        response = self.client.get('party.html')
+        response = self.client.get('/party.html')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'party.html')
         self.assertContains(response, 'Party setup')
