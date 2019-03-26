5ca5d630c86fb8fc89506a9886e9b07205047049
tllompart <tomeu@tllompart.es>
2019-03-26 11:22:49 +0100

    User Management 0.1

--- party/models.py
+++ party/models.py
@@ -1,3 +1,5 @@
+
+
 from django.db import models
 
 # Create your models here.
@@ -11,3 +13,7 @@ class Party(models.Model):
     fax = models.CharField(max_length=9)
     email = models.CharField(max_length=60)
     website = models.CharField(max_length=60)
+
+
+class PartyCategory(models.Model):
+    name = models.CharField(max_length=32)
